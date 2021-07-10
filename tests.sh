4952eec629618f192e2009b1520ae6b28131557d
Meir Gabay <unfor19@gmail.com>
2021-07-10 19:41:36 +0300

    added test

--- tests.sh
+++ tests.sh
@@ -28,16 +28,19 @@ should(){
 }
 
 # bargs_vars path - pass
-should pass "Default Values" "bash ./entrypoint.sh"
-should pass "Single Argument" "bash ./entrypoint.sh /"
-should pass "Two Arguments" "bash ./entrypoint.sh / 80"
-should pass "All Arguments" "bash ./entrypoint.sh / 75 92"
-should pass "Empty Values" "bash entrypoint.sh "" "" """
+should pass "Default Values"                 "bash ./entrypoint.sh"
+should pass "Single Argument"                "bash ./entrypoint.sh /"
+should pass "Two Arguments"                  "bash ./entrypoint.sh / 80"
+should pass "All Arguments"                  "bash ./entrypoint.sh / 75 92"
+should pass "Empty Values"                   "bash entrypoint.sh "" "" """
 export LOGGING_LEVEL=OFF
-should pass "Logging level - OFF" "bash entrypoint.sh"
+should pass "Logging level - OFF"            "bash entrypoint.sh"
 export LOGGING_LEVEL=DBG
-should pass "Logging level - Debugging" "bash entrypoint.sh / 75 92"
+should pass "Logging level - Debugging"      "bash entrypoint.sh / 75 92"
 export LOGGING_LEVEL=WRN
-should pass "Logging level - Warning"   "bash entrypoint.sh / 75 92"
+should pass "Logging level - Warning"        "bash entrypoint.sh / 75 92"
 export LOGGING_LEVEL=WILLY
-should fail "Logging level - Unknown"   "bash entrypoint.sh / 75 92"
+should fail "Logging level - Unknown"        "bash entrypoint.sh / 75 92"
+unset LOGGING_LEVEL
+export TEST_UNKNOWN_LEVEL=true
+should fail "Unknown inline logging level"   "bash entrypoint.sh / 75 92"
